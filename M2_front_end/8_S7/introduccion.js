868c0d4144e7820f2692355488ecf5ca0513f9e4
afranco <adrian.franco@edutecno.com>
2024-07-11 20:03:00 -0400

    variables javascript S7

---
+++ M2_front_end/8_S7/introduccion.js
@@ -0,0 +1,28 @@
+// este es un comentario de una sola línea
+
+// declaracion de variables en js
+let nombre = "Juan"; // la variable let es una variable local
+var edad = 20; // la variable var es una variable global
+const PI = 3.1416; // la variable const es una constante
+
+// tipos de datos
+var cadenaCaracteres = "Hola, Mundo"; // string
+var numero = 42; // number
+var esVerdadero = true; // boolean, true or false
+
+// objetos
+var persona = {
+    nombre: "Juan", // clave, valor
+    edad: 20
+}; 
+
+persona.nombre // acceder a la clave nombre
+
+// arreglo
+var colores = ["rojo", "verde", "azul"]; // va entre corchetes y cada uno de los elementos esta en una posición
+
+// imprimir en consola
+console.log("Hola, Mundo");
+
+// leer en una ventana emergente, con la funcion prompt(variable)
+var nombreIngresado = prompt("Ingrese su nombre");
