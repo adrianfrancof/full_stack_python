6a9990d63e453bfa6fff561c10485f26e7f4fda3
afranco <adrian.franco@edutecno.com>
2024-07-11 22:37:09 -0400

    enlace html con javascript S7

--- M2_front_end/8_S7/evaluacion/assets/js/script.js
+++ M2_front_end/8_S7/evaluacion/assets/js/script.js
@@ -23,11 +23,13 @@ switch (opcion) { //inicio switch case
     case "4":
         otrasConsultas();
         break;
-    default:
+    default: //caso por defecto que se ejecutara si no se cumple ningun caso
         alert("La opción ingresada no es válida.")
         break;
 } //fin switch case
 
+//estructura de la funcion
+// function nombreFuncion(parametros) {}
 function boletasPagos() { //inicio de la funcion
     var ingreso = prompt("Seleccione una opción\n"+
         "1.- Ver boleta\n"+
@@ -51,7 +53,7 @@ function reportarProblemas() { //inicio de la funcion
     if (ingreso == "1" || ingreso == "2") { // si ingreso es igual a 1 o 2
         var comentario = prompt("A continuación escriba su solicitud") 
         alert(`Estimado usuario, su solicitud: ${comentario}`)
-        alert("Estimado usuario, su solicitud: " + comentario)
+        alert("Estimado usuario, su solicitud: " + comentario) // otra forma de concatenar
     } else {
         alert("Opción ingresada no es valida")
     }
@@ -76,7 +78,5 @@ function ofertaComercial() { //inicio de la funcion
 
 function otrasConsultas() { //inicio de la funcion
     var ingreso = prompt("A continuación escriba su consulta")
-    alert("Estimado usuario, su consulta: " + ingreso +
-        " Ha sido ingresada con éxito. Pronto será contactado por uno de nuestros ejecutivos"
-    )
+    alert("Estimado usuario, su consulta: " + ingreso +" Ha sido ingresada con éxito. Pronto será contactado por uno de nuestros ejecutivos")
 } //fin de la funcion
