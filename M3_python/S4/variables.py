9a18f9e10ae685eaf586b566a3302898e67ad076
afranco <adrian.franco@edutecno.com>
2024-08-01 20:10:52 -0400

    variables globales y locales

---
+++ M3_python/S4/variables.py
@@ -0,0 +1,21 @@
+# las variables de declaran con un nombre, que es su identificador
+#van escritas en snake_case
+#identificador = valor
+nombre_variable = 'una variable'
+nombre_variable = 2 #sustituye el valor de la variable
+
+print(nombre_variable) #2
+
+# variables locales y variables globales
+s = 'una variable global'
+
+#las variables locales existen dentro de los bloques de codigo
+# funciones, condicionales (if, else if), bucles (ciclos)
+def f(s):
+    s = 'sustituimos la variable global'
+    variable_local = 'una variable local'
+    print(s)
+    
+f(s) #invocando funcion f
+#print(variable_local) #error, variable local no definida
+print(s) #una variable global
