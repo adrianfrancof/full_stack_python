e71f94047957734f0118c669b9f20e2d67bb528a
afranco <adrian.franco@edutecno.com>
2024-08-09 21:02:25 -0400

    ciclo for

---
+++ M3_python/S5/ciclo_for.py
@@ -0,0 +1,29 @@
+# ciclo for o ciclo para
+
+# for variable in secuencia:
+
+#for i in data:
+    #instrucciones a ejecutar
+    
+frutas = ['manzana', 'pera', 'mango', 'fresa', 'kiwi']
+
+# conocemos el elemento
+for i in frutas: # for each
+    print(i)
+    
+# for i in range(len(frutas)):
+for i in range(1,6):
+    print(i)
+
+# len(frutas) = 5        
+for i in range(len(frutas)):
+    print(i)
+
+#conocemos el elemento y el indice o posición    
+for i in range(len(frutas)):
+    print(frutas[i])
+
+# recorrido de una cadena de texto
+palabra = 'Python'    
+for i in range(len(palabra)):
+    print(palabra[i])               
